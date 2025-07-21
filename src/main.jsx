ffe2cefcb2ad682045feaefa419554855cf8b8e9
Supriya12200 <namasupriyanama@gmail.com>
2025-07-21 14:56:41 +0530

    v1

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Myapp from './Myapp.jsx'
-import{} from 'react-router-dom'
-
+import{ BrowserRouter, Route, Routes } from 'react-router-dom'
+import Welcome from './Welcome.jsx'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <Routes>
-<Route path="app" element={<App/>}></Route>
-  <Route path="myapp" element={<Myapp/>}></Route>
-  <Route path='/' element
-
+    <Route>
+      <Route path="app" element={<App/>}></Route>
+      <Route path='myapp' element={<Myapp/>}></Route>
+      <Route path='/' element={<Welcome/>}></Route> 
+    </Route>
   </Routes>
-
-</BrowserRouter>
+  </BrowserRouter>
+    
 )
