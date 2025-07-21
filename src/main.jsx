806d2b7aa710d647cb7c545eab13d43f77815da4
Supriya12200 <namasupriyanama@gmail.com>
2025-07-21 15:28:47 +0530

    v1

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import App from './App';
+import Myapp from './Myapp';
+import Welcome from './Welcome';
 import './index.css'
-import App from './App.jsx'
-import Myapp from './Myapp.jsx'
-import{ BrowserRouter, Route, Routes } from 'react-router-dom'
-import Welcome from './Welcome.jsx'
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <Routes>
-    <Route>
-      <Route path="app" element={<App/>}></Route>
-      <Route path='myapp' element={<Myapp/>}></Route>
-      <Route path='/' element={<Welcome/>}></Route> 
-    </Route>
-  </Routes>
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter basename='f12'>
+    <Routes>
+      <Route path="/" element={<Welcome />} />
+      <Route path="app" element={<App />} />
+      <Route path="myapp" element={<Myapp />} />
+    </Routes>
   </BrowserRouter>
-    
-)
+);
